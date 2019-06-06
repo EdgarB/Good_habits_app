a04b3c2e342a05e160d18b22d92fc5fa0ebf998b
Edgar <A01113146@itesm.mx>
2019-06-06 16:32:47 -0500

    Now logs of specific activity can be seen

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,16 @@
 class LogsController < ApplicationController
     before_action :authenticate_user!
     def index
+        current_act = Activity.find(params[:activity_id]);
+        if(current_act)
+            @act_name = current_act.name
+            @logs = current_act.logs
+        else
+            redirect_to root_path, alert: "That habit doesn't exist"
+        end
     end
-  def new
+
+    def new
       current_act = Activity.find(params[:activity_id]);
       if(current_act)
           @log = Log.new
@@ -10,20 +18,20 @@ class LogsController < ApplicationController
       else
           redirect_to root_path
       end
-  end
+    end
 
-  def create
+    def create
       log_to_save = Log.new
-      log_to_save.description = params[:log][:done_at]
+      log_to_save.description = params[:log][:description]
       log_to_save.done_at = params[:log][:done_at]
       log_to_save.activity = Activity.find(params[:activity_id]);
       if(log_to_save.save)
 
-           redirect_to root_path , :notice => "Your log was saed succesfully."
+           redirect_to root_path , notice: "Your log was added succesfully."
       else
-          redirect_to root_path , :alert => "Couldn't save your log ..."
+          redirect_to root_path , alert: "Couldn't save your log ..."
       end
 
 
-  end
+    end
 end
